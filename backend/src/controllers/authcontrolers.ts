7f35c7725ec0191eba2575e5fe8d58b786016400
leoduarte2005 <leonardocanalez0@gmail.com>
2025-04-06 17:08:10 -0300

    backend do login

---
+++ backend/src/controllers/authcontrolers.ts
@@ -0,0 +1,53 @@
+import { Request, Response } from "express";
+import { loginService } from "../services/AuthService";
+
+/**
+ * @swagger
+ * /login:
+ *   post:
+ *     summary: Realiza o login de um usuário
+ *     tags:
+ *       - Autenticação
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - register
+ *               - password
+ *             properties:
+ *               register:
+ *                 type: string
+ *                 example: 117276
+ *               password:
+ *                 type: string
+ *                 format: password
+ *                 example: "123456"
+ *     responses:
+ *       200:
+ *         description: Login bem-sucedido, retorna o token JWT
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 token:
+ *                   type: string
+ *                   example: "jwt_token_aqui"
+ *       400:
+ *         description: Credenciais inválidas
+ *       500:
+ *         description: Erro interno no servidor
+ */
+export const loginEmployee = async (req: Request, res: Response) => {
+  const { register, password } = req.body;
+
+  try {
+    const token = await loginService({ register, password });
+    res.status(200).json({ token });
+  } catch (error: any) {
+    res.status(400).json({ error: error.message });
+  }
+};
